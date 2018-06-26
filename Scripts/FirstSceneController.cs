0ef7e5727427a9df491afcdc9a599b5b23f45dc3
MapleLai <37803093+MapleLai@users.noreply.github.com>
2018-06-27 00:16:33 +0800

    Add files via upload

---
+++ Scripts/FirstSceneController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstSceneController : MonoBehaviour {
+
+	private static FirstSceneController _instance;
+	public GameObject[] players;
+	public int playersCount;
+	public int gameOver;
+	public float time;
+
+	public static FirstSceneController getInstance() {
+		return _instance;
+	}
+		
+	void Awake(){
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		gameOver = -1000;
+		time = 0;
+		players = new GameObject[2];
+		playersCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (time > 0)
+			time -= Time.deltaTime;
+		if (time < 0)
+			gameOver = 1;
+	}
+
+	public void addPlayer(){
+		playersCount++;
+		if (playersCount == 2)
+			time = 30f;
+	}
+
+	public void setGameOver(int go){
+		gameOver = go;
+	}
+
+	void OnGUI(){
+		if (FirstSceneController.getInstance().gameOver == -1) {
+			GUIStyle style = new GUIStyle();
+			style.normal.background = null;
+			style.normal.textColor = new Color(0, 0, 0);
+			style.fontSize = 40;
+			GUI.TextField (new Rect (200, 150, 300, 50), "Player2 Wins!", style);
+		}
+		if (FirstSceneController.getInstance().gameOver == 1) {
+			GUIStyle style = new GUIStyle();
+			style.normal.background = null;
+			style.normal.textColor = new Color(0, 0, 0);
+			style.fontSize = 40;
+			GUI.TextField (new Rect (200, 150, 300, 50), "Player1 Wins!", style);
+		}
+	}
+
+}
---
+++ Scripts/UserInterface.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class UserInterface : NetworkBehaviour {
+	// Use this for initialization
+
+	void Start () {
+		if(isLocalPlayer)
+			CmdAddPlayer();
+	}
+
+	void Update () {
+		if (!isLocalPlayer)
+			return;
+
+		float translationX = Input.GetAxis ("Horizontal") * 6f;
+		float translationZ = Input.GetAxis ("Vertical") * 6f;
+		translationX *= Time.deltaTime;
+		translationZ *= Time.deltaTime;
+		this.transform.Translate (translationX, 0, 0);
+		this.transform.Translate (0, 0, translationZ);
+	}
+
+	[Command]
+	void CmdAddPlayer(){
+		FirstSceneController.getInstance().addPlayer();
+	}
+
+	void OnCollisionStay(Collision other){
+		if (other.gameObject.tag == "Player")
+			FirstSceneController.getInstance().setGameOver(-1);
+	}
+
+}
